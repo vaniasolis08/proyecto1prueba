1a3f0d0798d73c853802b1ceacd634bb2003fcc6
vania solis <solisvictoria924@gmail.com>
2024-07-09 07:31:55 -0600

    creando repositorio

---
+++ src/proyecto/de/git/pkg1/ProyectoDeGit1.java
@@ -0,0 +1,15 @@
+
+package proyecto.de.git.pkg1;
+
+
+public class ProyectoDeGit1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("mi proyecto");
+        for (int i = 0; i < 10; i++) { 
+            System.out.println(i);
+        }
+    }
+    
+}
